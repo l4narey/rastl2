c8d7949ed94eff1e9d1f20212319b6f2bd7195e8
l4narey <l4na011s@gmail.com>
2025-02-06 15:52:57 -0300

    Adicionando botão para obter endereço

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+document.getElementById("obterEndereco").addEventListener("click", function() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocalização não é suportada neste navegador.");
+    }
+});
+
 function showPosition(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
 
     // Substitua pela sua chave da API
-    var apiKey = "AIzaSyDuQtrZDbCsLW8AeEYo6ZqXnzeqi2Mq6EA";
+    var apiKey = "SUA_CHAVE_DE_API_DO_GOOGLE_MAPS";
     
     var url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`;
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            // Verifica se o retorno da API contém algum resultado
             if (data.results.length > 0) {
                 var endereco = data.results[0].formatted_address;
                 document.getElementById("localizacao").innerHTML = "Endereço: " + endereco;
